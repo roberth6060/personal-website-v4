cff8333dbd0f622f781163c9f42323e86dbdeccc
Robert Haye <roberthaye@Roberts-MacBook-Pro.local>
2022-10-13 01:17:29 +0200

    updated hmtl structure and style

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,21 +10,57 @@ const timeline = document.querySelector(".timeline");
 const schoolData = [
   {
     name: "RIT Croatia",
-    content:
-      "RIT <br/> is globally known for cooperative education and has one of the oldest and biggest co-op programs in the world. It annually places more than 4,100 students in more than 6,100 co-op assignments with nearly 2,100 employers across the United States and overseas.",
+    content: `
+    <ul>
+        <li>Regionally Accredited</li>
+        <li>Accredited by: the Middle States Commission on Higher Education</li>
+    </ul>
+    <h3>Random Facts</h3>
+    <ul>
+        <li>The American College of Management and Technology (ACMT) was formed in 1995</li>
+        <li>Rochester Institute of Technology and the Ministry of Science sign a cooperation agreement making ACMT the first and only private college in Croatia granting dual degrees for undergraduate programs</li>
+        <li>RIT is located in Dubrovnik, and in Zagreb, Croatia</li>
+        <li>The following programs are offered at at RIT:
+            <ol>
+            <li>Msc. Service Leadership and Innovation</li>
+            <li>Bsc. Tourism and Hospitality</li>
+            <li>Bsc. Information Technology / Web and Mobile Computing</li>
+            <li>International Business</li>
+            </ol>
+        </li>
+    </ul>`,
     img: "https://pbs.twimg.com/profile_images/1171468021900034058/Gu1RX0aR_400x400.jpg",
   },
 
   {
     name: "Chapman Unversity",
-    content:
-      "#4 of the Top American Film Schools, Dodge College of Film and Media Arts – The Hollywood Reporter. #52 (tied) in Most Innovative Schools - U.S. News and World Report. #76 (tied) in Best Colleges for Veterans – U.S. News and World Report.",
+    content: `<ul>
+        <li>Regionally Accredited</li><li>Accredited by: the Western Association of Schools and Colleges, Accrediting Commission for Senior Colleges and Universities</li>
+      </ul>
+      <h3>Random Facts</h3>
+      <ul>
+        <li>Private research university in Orange, California</li>
+        <li>Founded in 1861 in Woodland, California, by the Christian Church (Disciples of Christ) under the name Hesperian College</li>
+        <li>Chapman University and Anglo-American University have partnered to offer a unique opportunity for students to earn an MBA in just 17 months in Prague</li>
+        <li>Courses are delivered in an intensive weekend format, this 46-credit MBA program is taught by both Chapman faculty and AAU faculty</li>
+      </ul>
+      `,
     img: "https://brand.chapman.edu/wp-content/uploads/2015/01/cu-seal.jpg",
   },
   {
     name: "American Military Unversity",
-    content:
-      "American Military University is part of American Public University System (APUS), which is accredited by the Higher Learning Commission (HLC), an institutional accreditation agency recognized by the U.S. Department of Education.",
+    content: `<ul>
+        <li>Regionally Accredited</li>
+        <li>Accredited by: the Higher Learning Commission</li>
+      </ul>
+      <h3>Random Facts</h3>
+      <ul>
+        <li>In 1991, retired Marine Corps officer James P. Etter founded American Military University</li>
+        <li>In 2002, AMU was reorganized into the American Public University System</li>
+        <li>Today the university system is one of the largest providers of online higher education offering more than 190 academic programs to students enrolled worldwide</li>
+        <li>Military Times recognizes the University with a “Best for Vets Colleges” distinction</li>
+      </ul>
+      `,
     img: "https://www.collegeconsensus.com/wp-content/uploads/2018/12/american-military-university-logo-168449.jpg",
   },
 ];
@@ -70,6 +106,8 @@ timeline.addEventListener("click", (e) => {
     const [school] = schoolData.filter((school) => school.name === schoolName);
     modalTitle.textContent = school.name;
     modalImg.src = school.img;
-    modalBody.innerHTML = school.content;
+    modalBody.innerHTML = `
+    <h2>Important Information</h2>
+    <hr width ="50%"/><h3>Accreditation</h3>${school.content}`;
   }
 });
